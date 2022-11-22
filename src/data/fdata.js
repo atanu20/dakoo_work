233573ee99c8898ec4e7b85486df39a32544af1a
atanu <atanuj383@gmail.com>
2022-11-22 13:27:22 +0530

    Add

--- src/data/fdata.js
+++ src/data/fdata.js
@@ -1,3 +1,4 @@
 // export const apilink = 'http://localhost:5000';
 
-export const apilink = 'https://bookapp-aj.herokuapp.com';
+// export const apilink = 'https://bookapp-aj.herokuapp.com';
+export const apilink = 'https://dakoo-book-node.onrender.com';
